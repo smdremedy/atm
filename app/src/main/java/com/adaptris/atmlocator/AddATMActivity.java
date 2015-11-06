59b3eb883dced94e3e2f5b2ef972c4fc4c15d09b
Sylwester Madej <sylwek.madej@gmail.com>
2015-11-06 12:15:33 +0100

    Save state

--- app/src/main/java/com/adaptris/atmlocator/AddATMActivity.java
+++ app/src/main/java/com/adaptris/atmlocator/AddATMActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -18,6 +19,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
 import butterknife.Bind;
@@ -32,10 +34,18 @@ public class AddATMActivity extends AppCompatActivity implements
     private static final int REQUEST_PLACE_PICKER = 123;
     private static final String LOG_TAG = AddATMActivity.class.getSimpleName();
     private static final int RESOLUTION_REQUEST_CODE = 321;
+    public static final String ATM = "atm";
     @Bind(R.id.pickAddressButton)
     Button pickAddressButton;
+    @Bind(R.id.placeNameTextView)
+    TextView placeNameTextView;
+    @Bind(R.id.placeAddressTextView)
+    TextView placeAddressTextView;
+    @Bind(R.id.placeLatLongTextView)
+    TextView placeLatLongTextView;
     private GoogleApiClient googleApiClient;
     private LocationRequest locationRequest;
+    private AtmPlace atmPlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,7 +102,7 @@ public class AddATMActivity extends AppCompatActivity implements
     @Override
     protected void onPause() {
         super.onPause();
-        if(googleApiClient.isConnected()) {
+        if (googleApiClient.isConnected()) {
             LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
             googleApiClient.disconnect();
         }
@@ -102,7 +112,7 @@ public class AddATMActivity extends AppCompatActivity implements
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
 
-        if(connectionResult.hasResolution()) {
+        if (connectionResult.hasResolution()) {
             try {
                 connectionResult.startResolutionForResult(this, RESOLUTION_REQUEST_CODE);
             } catch (IntentSender.SendIntentException e) {
@@ -117,6 +127,41 @@ public class AddATMActivity extends AppCompatActivity implements
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+
+        if (requestCode == REQUEST_PLACE_PICKER && resultCode == RESULT_OK) {
+            Place place = PlacePicker.getPlace(data, this);
+
+            atmPlace = new AtmPlace();
+            atmPlace.name = place.getName().toString();
+            atmPlace.address = place.getAddress().toString();
+            atmPlace.lat = place.getLatLng().latitude;
+            atmPlace.lng = place.getLatLng().longitude;
+
+            showAtmPlace();
+
+
+
+        }
+    }
+
+    private void showAtmPlace() {
+        placeNameTextView.setText(atmPlace.name);
+        placeAddressTextView.setText(atmPlace.address);
+        placeLatLongTextView.setText(String.format("%f,%f", atmPlace.lat, atmPlace.lng));
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(ATM, atmPlace);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        atmPlace = (AtmPlace) savedInstanceState.getSerializable(ATM);
+        showAtmPlace();
     }
 
     @Override
@@ -125,7 +170,7 @@ public class AddATMActivity extends AppCompatActivity implements
 
         Log.d(LOG_TAG, "Connected");
         Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-        if(lastLocation != null) {
+        if (lastLocation != null) {
             handleLocation(lastLocation);
         } else {
             LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
---
+++ app/src/main/java/com/adaptris/atmlocator/AtmPlace.java
@@ -0,0 +1,14 @@
+package com.adaptris.atmlocator;
+
+import java.io.Serializable;
+
+/**
+ * Created by Sylwester on 2015-11-06.
+ */
+public class AtmPlace implements Serializable {
+
+    public String name;
+    public String address;
+    public double lat;
+    public double lng;
+}
